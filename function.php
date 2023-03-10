c21b59de0444cb01d252b566ee943157a97d5ea9
rusparesitaputri <resitaputriruspa@gmail.com>
2023-03-10 15:51:48 +0700

    ini praktikum3

---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+
+function hitungUmur($tahunLahir = 2002, $tahunSekarang = 2023){
+echo $tahunSekarang - $tahunLahir;
+}
+
+hitungUmur(2002,2023);
+echo "<br>";
+hitungUmur(1990,2023);
+echo "<br>";
+hitungUmur(1918,2023);
+echo "<br>";
+hitungUmur();
+echo "<br>";
+
+//fungsi kedua
+function greeting($nama)
+{
+    return "hai, namaku $nama !";
+}
+echo greeting("Ruspa");
